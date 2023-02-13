f7c11db65b5763782793ad8b90dbbead5dcf296e
itswillum <williamhangouts@gmail.com>
2023-02-12 19:05:44 -0500

    prep for routing project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,5 @@
-import Todo from "./Todo";
 function App() {
-  return (
-    <div>
-      <h1>Todos</h1>
-      <Todo text='1' />
-      <Todo text='2' />
-      <Todo text='3' />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
